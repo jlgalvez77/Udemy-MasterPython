95842fc730c67639273b0f82a95f0a71bd6a81fd
Jose Galvez <jlgalvez77@gmail.com>
2025-05-22 18:01:09 +0200

    Seccion 03 desde el portatil

---
+++ 03 - Funciones/intro_funciones.py
@@ -0,0 +1,12 @@
+def texto_bienvenida():
+    print('Hola Python')
+    print('Es el curso de Programación')
+    print('Para crear paginas y juegos')
+
+def suma_numeros():
+    pass
+
+# si no invoco la funcion no se ejecuta
+texto_bienvenida()
+suma_numeros()
+# si no invoco la funcion no se ejecuta 
---
+++ 03 - Funciones/parametros_funciones.py
@@ -0,0 +1,13 @@
+def imprimir(nombre_completo):
+    print(nombre_completo)
+
+imprimir("Juan Pérez")
+imprimir("Ana Gómez")
+imprimir("Luis Rodríguez")
+
+def sumar(a, b):
+    c = a + b
+    print(f"La suma de {a} y {b} es: {c}")
+
+sumar(5, 10)
+sumar(3, 7)
